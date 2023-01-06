83a5a7a0da64f5f42da645ee7e51336378f9974c
Abecedarian <106958470+Aartimehr@users.noreply.github.com>
2023-01-06 22:31:34 +0530

    Create Recalling.cpp

---
+++ Recalling.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int i;
+    cout<<"table of 9";
+    int n=9;
+    for(i=1;i<=10;i++)
+      {
+        cout<<i*n<<endl;
+    
+    }
+
+    
+    return 0;
+}
